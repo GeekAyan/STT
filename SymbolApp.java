c050d5e7b63c9f44062b72a3494279e9f7979edd
Mazid Nawaz Ahmad <mazidnawaz92@gmail.com>
2024-08-14 19:56:44 +0530

    Button Changed to "Chin Tapak Dum Dum"

--- SymbolApp.java
+++ SymbolApp.java
@@ -41,7 +41,8 @@ public class SymbolApp extends Frame implements ActionListener {
 
         // Panel for submit button
         Panel controlPanel = new Panel(new FlowLayout());
-        submitButton = new Button("Submit");
+        submitButton = new Button("Chin Tapak Dum Dum");
+        submitButton.setPreferredSize(new Dimension(200, 50));
         submitButton.addActionListener(this);
         controlPanel.add(submitButton);
         add(controlPanel, BorderLayout.SOUTH);
