b0b395e7e07d3f61820dfbbdde7db56b7ae7a389
Soumilidas1234 <soumilid91@gmail.com>
2024-08-23 12:40:36 +0530

    Update SymbolApp.java

--- SymbolApp.java
+++ SymbolApp.java
@@ -41,7 +41,11 @@ public class SymbolApp extends Frame implements ActionListener {
 
         // Panel for submit button
         Panel controlPanel = new Panel(new FlowLayout());
-        submitButton = new Button("Submit");
+        submitButton = new Button("chin tapak dum dum");
+        submitButton.setFont(new Font("Courier", Font.PLAIN, 14)); 
+        submitButton.setPreferredSize(new Dimension(150, 50));
+        submitButton.setFont(new Font("Courier", Font.PLAIN, 20)); 
+        submitButton.setPreferredSize(new Dimension(200, 50));
         submitButton.addActionListener(this);
         controlPanel.add(submitButton);
         add(controlPanel, BorderLayout.SOUTH);
